7dd6bb5afa58056d3c60bf247f84acdcfaf47b5b
rocio <rocio@DESKTOP-DEGQ0JQ>
2024-05-15 03:06:53 -0300

    .

--- Tp2_RamosRocio/src/ar/edu/unju/fi/ejercicio3/model/Provincia.java
+++ Tp2_RamosRocio/src/ar/edu/unju/fi/ejercicio3/model/Provincia.java
@@ -1,5 +1,39 @@
 package ar.edu.unju.fi.ejercicio3.model;
 
-public class Provincia {
 
+public enum Provincia {
+	JUJUY(1000000, 53223), // Valores inventados
+    SALTA(1500000, 75432),
+    TUCUMAN(1200000, 64321),
+    CATAMARCA(800000, 42123),
+    LA_RIOJA(600000, 31234),
+    SANTIAGO_DEL_ESTERO(1300000, 68213);
+
+	private int cantidadPoblacion;
+    private int superficie;
+	
+	Provincia(int cantidadPoblacion, int superficie) {
+        this.cantidadPoblacion = cantidadPoblacion;
+        this.superficie = superficie;
+    }
+
+    public int getCantidadPoblacion() {
+        return cantidadPoblacion;
+    }
+
+    public void setCantidadPoblacion(int cantidadPoblacion) {
+        this.cantidadPoblacion = cantidadPoblacion;
+    }
+
+    public int getSuperficie() {
+        return superficie;
+    }
+
+    public void setSuperficie(int superficie) {
+        this.superficie = superficie;
+    }
+
+    public double calcularDensidadPoblacional() {
+        return (double) cantidadPoblacion / superficie;
+    }
 }
