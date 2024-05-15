1fd2fbe5b3c0a2cb2eb3cf35c74d81c8a816cbe5
rocio <rocio@DESKTOP-DEGQ0JQ>
2024-05-15 03:37:07 -0300

    Final del punto 6

---
+++ Tp2_RamosRocio/src/ar/edu/unju/fi/ejercicio6/inerfaces/funcionales/convert.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.ejercicio6.inerfaces.funcionales;
+
+public class convert {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Tp2_RamosRocio/src/ar/edu/unju/fi/ejercicio6/model/FelinoSalvaje.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.ejercicio6.model;
+
+public class FelinoSalvaje {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
